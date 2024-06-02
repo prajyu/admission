9cd9a5af9f45f95473994f9d85910b4ddfd5464f
Prajyu M S <69985630+prajyu@users.noreply.github.com>
2024-06-02 19:15:49 +0530

    Files

---
+++ app-ad.js
@@ -0,0 +1,110 @@
+function allotment(){
+    const allotment = document.querySelector('#allotment');
+    const plus1 = document.querySelector('.plus1');
+    const minus1 = document.querySelector('.minus1');
+
+        allotment.classList.toggle("active");
+        plus1.classList.toggle("inactive");
+        minus1.classList.toggle("active");
+}
+function Scholar(){
+    const Scholar = document.querySelector('#Scholar');
+    const plus9 = document.querySelector('.plus9');
+    const minus9 = document.querySelector('.minus9');
+    
+        Scholar.classList.toggle("active");
+        plus9.classList.toggle("inactive");
+        minus9.classList.toggle("active");
+}
+
+
+
+function lastrank(){
+    const lastrank = document.querySelector('#lastrank');
+    const plus2 = document.querySelector('.plus2');
+    const minus2 = document.querySelector('.minus2');
+
+        lastrank.classList.toggle("active");
+        plus2.classList.toggle("inactive");
+        minus2.classList.toggle("active");
+}
+
+function documents(){
+    const documents = document.querySelector('#documents');
+    const plus3 = document.querySelector('.plus3');
+    const minus3 = document.querySelector('.minus3');
+
+        documents.classList.toggle("active");
+        plus3.classList.toggle("inactive");
+        minus3.classList.toggle("active");
+}
+
+function hostel(){
+    const hostel = document.querySelector('#hostel');
+    const plus4 = document.querySelector('.plus4');
+    const minus4 = document.querySelector('.minus4');
+
+        hostel.classList.toggle("active");
+        plus4.classList.toggle("inactive");
+        minus4.classList.toggle("active");
+}
+
+function union(){
+    const union = document.querySelector('#union');
+    const plus5 = document.querySelector('.plus5');
+    const minus5 = document.querySelector('.minus5');
+
+        union.classList.toggle("active");
+        plus5.classList.toggle("inactive");
+        minus5.classList.toggle("active");
+}
+
+function keep(){
+    const keep = document.querySelector('#keep');
+    const plus6 = document.querySelector('.plus6');
+    const minus6 = document.querySelector('.minus6');
+
+        keep.classList.toggle("active");
+        plus6.classList.toggle("inactive");
+        minus6.classList.toggle("active");
+}
+
+function mens(){
+    const mens = document.querySelector('#mens');
+    const plusm = document.querySelector('.plusm');
+    const minusm = document.querySelector('.minusm');
+
+        mens.classList.toggle("active");
+        plusm.classList.toggle("inactive");
+        minusm.classList.toggle("active");
+}
+
+function ladies(){
+    const ladies = document.querySelector('#ladies');
+    const plusl = document.querySelector('.plusl');
+    const minusl = document.querySelector('.minusl');
+
+        ladies.classList.toggle("active");
+        plusl.classList.toggle("inactive");
+        minusl.classList.toggle("active");
+}
+
+function pages(){
+    const pages = document.querySelector('#pages');
+    const plus7 = document.querySelector('.plus7');
+    const minus7 = document.querySelector('.minus7');
+
+        pages.classList.toggle("active");
+        plus7.classList.toggle("inactive");
+        minus7.classList.toggle("active");
+}
+
+function activities(){
+    const activities = document.querySelector('#activities');
+    const plus8 = document.querySelector('.plus8');
+    const minus8 = document.querySelector('.minus8');
+
+        activities.classList.toggle("active");
+        plus8.classList.toggle("inactive");
+        minus8.classList.toggle("active");
+}
